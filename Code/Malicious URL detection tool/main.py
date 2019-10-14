6d73e768b9e337ddb4a289fe06728046d83aec13
Retro Developer <54714954+retrde@users.noreply.github.com>
2019-10-14 20:37:13 +0530

    changing order
    - for instruction pointer restricting redundant exectutions

--- Code/Malicious URL detection tool/main.py
+++ Code/Malicious URL detection tool/main.py
@@ -1,23 +1,25 @@
 import pandas as pd
 import numpy as np
 import sklearn
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.cross_validation import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import TfidfVectorizer
+
 import random
 from tkinter import *
 from tkinter import ttk 
 import time
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 boolean= False
 to_ret=[]
 
 
 def getTkns(input):
-	 tknsBySlash= str(input.encode('utf-8')).split('/')
-	 allTkns= []
-	 for i in tknsBySlash:
+	tknsBySlash= str(input.encode('utf-8')).split('/')
+	allTkns= []
+	for i in tknsBySlash:
 		 tkns= str(i).split('-')
 		 tknsByDot= []
 		 for j in range(0, len(tkns)):
@@ -32,78 +34,78 @@ def getTkns(input):
 
 
 def trainer():
-    global to_ret
-    global txt
-	
-    # ONE TIME EXECUTION STARTS HERE
-    csv= pd.read_csv("data.csv", error_bad_lines= False)
-    data= pd.DataFrame(csv)
-    ```
-    data= np.array(data)
-    random.shuffle(data)
-    
-    y= [d[1] for d in data]
-    corp= [d[0] for d in data]
-    
-    count_vectorizer= CountVectorizer(tokenizer= getTkns)
-    tf_vectorizer= TfidfVectorizer(tokenizer= getTkns)
-     
-    X1= count_vectorizer.fit_transform(corp)
-    X2= tf_vectorizer.fit_transform(corp)
-    
-    # Fit the Logistic Regression Model
-    X1_train, X1_test, y1_train, y1_test= train_test_split(X1, y, test_size= 0.2, random_state= 42)
-    X2_train, X2_test, y2_train, y2_test= train_test_split(X2, y, test_size= 0.2, random_state= 42)
-    lgs_count= LogisticRegression()
-    lgs_tf   = LogisticRegression()
-    lgs_count.fit(X1_train, y1_train)
-    lgs_tf.fit(X2_train,y2_train)
+	global to_ret
+	global txt
 
-    out1= "The accuracy of Model with Count Vectorizer is "+str(lgs_count.score(X1_test, y1_test))+"\n"
-    out2= "The accuracy of Model with TFIDF Vectorizer is "+str(lgs_tf.score(X2_test, y2_test))+"\n"
-    txt.insert(0.0,out1)
-    txt.insert(1.0,out2)
+	# ONE TIME EXECUTION STARTS HERE
+	csv= pd.read_csv("data.csv", error_bad_lines= False)
+	data= pd.DataFrame(csv)
+	```
+	data= np.array(data)
+	random.shuffle(data)
+
+	y= [d[1] for d in data]
+	corp= [d[0] for d in data]
+
+	count_vectorizer= CountVectorizer(tokenizer= getTkns)
+	tf_vectorizer= TfidfVectorizer(tokenizer= getTkns)
+
+	X1= count_vectorizer.fit_transform(corp)
+	X2= tf_vectorizer.fit_transform(corp)
+
+	# Fit the Logistic Regression Model
+	X1_train, X1_test, y1_train, y1_test= train_test_split(X1, y, test_size= 0.2, random_state= 42)
+	X2_train, X2_test, y2_train, y2_test= train_test_split(X2, y, test_size= 0.2, random_state= 42)
+	lgs_count= LogisticRegression()
+	lgs_tf   = LogisticRegression()
+	lgs_count.fit(X1_train, y1_train)
+	lgs_tf.fit(X2_train,y2_train)
+
+	out1= "The accuracy of Model with Count Vectorizer is "+str(lgs_count.score(X1_test, y1_test))+"\n"
+	out2= "The accuracy of Model with TFIDF Vectorizer is "+str(lgs_tf.score(X2_test, y2_test))+"\n"
+	txt.insert(0.0,out1)
+	txt.insert(1.0,out2)
 
 
-    to_ret.append(count_vectorizer)
-    to_ret.append(tf_vectorizer)
-    to_ret.append(lgs_count)
-    to_ret.append(lgs_tf)
-    return to_ret
+	to_ret.append(count_vectorizer)
+	to_ret.append(tf_vectorizer)
+	to_ret.append(lgs_count)
+	to_ret.append(lgs_tf)
+	return to_ret
 
 
      
 
 def callback():
 
-    global boolean
-    global to_ret
-    global txt
-    global choice
+	global boolean
+	global to_ret
+	global txt
+	global choice
 
-    txtname= entry.get()
-    if txtname=="":
-        txt.insert(0.0,"Enter valid URL\n")
-        return
-    Choice= choice.get()
-    if Choice == "":
-        txt.insert(0.0,"Please choose any one of the two options.\n")
-        return
+	txtname= entry.get()
+	if txtname=="":
+		txt.insert(0.0,"Enter valid URL\n")
+		return
+	Choice= choice.get()
+	if Choice == "":
+		txt.insert(0.0,"Please choose any one of the two options.\n")
+		return
 
 
-    X_predict= [txtname]
-    if Choice=="CV":        
-        X_predict= to_ret[0].transform(X_predict)
-        y_predict= to_ret[2].predict(X_predict)
-        out= str(txtname)+" is found "+str(y_predict)+"\n"
-        txt.insert(0.0, out)
-    if Choice=="TF":        
-        X_predict= to_ret[1].transform(X_predict)
-        y_predict= to_ret[3].predict(X_predict)
-        out= str(txtname)+" is found "+str(y_predict)+"\n"
-        txt.insert(0.0, out)
-        
-    return
+	X_predict= [txtname]
+    	if Choice=="CV":        
+		X_predict= to_ret[0].transform(X_predict)
+		y_predict= to_ret[2].predict(X_predict)
+		out= str(txtname)+" is found "+str(y_predict)+"\n"
+		txt.insert(0.0, out)
+    	if Choice=="TF":        
+		X_predict= to_ret[1].transform(X_predict)
+		y_predict= to_ret[3].predict(X_predict)
+		out= str(txtname)+" is found "+str(y_predict)+"\n"
+		txt.insert(0.0, out)
+
+  	return
     	
 
 
@@ -122,9 +124,11 @@ label.pack()
 
 label2= ttk.Label(frame, text='\n\n')
 label2.pack()
+
 ttk.Radiobutton(frame,text='Use Count Vectorizer',variable=choice,value='CV').pack(anchor='w')
 label3= ttk.Label(frame, text='\n')
 label3.pack()
+
 ttk.Radiobutton(frame,text='Use Tf-Idf Vectorizer',variable=choice,value='TF').pack(anchor='w')
 label4= ttk.Label(frame, text='\n')
 label4.pack()
